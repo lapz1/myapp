8f0dd75e7c155d3aed2b8f6c597633f30babb5ba
Luis <lapz01@gmail.com>
2020-04-16 20:32:27 -0500

    Carga del desarrollo en faker

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const iva = require("./modules/iva");
 const log = require("./modules/logs");
+//third party modules
+const faker = require("faker");
 
 console.log(`el valor del iva es ${iva.iva}%`);
 log.info(`el valor del iva vehiculos es ${iva.ivaVehiculos}%`);
 log.info(`el valor del iva canasta familiar es ${iva.ivaCanastaFamiliar}%`);
+
+log.info(faker.name.findName());
